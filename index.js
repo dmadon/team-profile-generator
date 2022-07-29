527db1d46f19fb6afc4ce9c5af8dc6bf58c70c93
dmadon <deanna.madon@gmail.com>
2022-07-28 23:50:16 -0500

    start work on user input prompts

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log('hello, deanna')
+const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+function promptUser(){
+    inquirer
+        .prompt({
+            type: 'input',
+            name: 'teamName',
+            message: 'Please enter your team name.'
+        });
+};
+
+function managerInfo(){
+    inquirer
+        .prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: "Please enter the team manager's name.",
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: "Please enter the team manager's ID number."
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "Please enter the team manager's email address."
+        },
+        {
+            type:'input',
+            name: 'officeNum',
+            message: "Please enter the team manager's office number."
+        }
+        ])
+        .then((managerInfo)=>{
+            console.log(managerInfo);
+        })
+};
+
+
+
+
+promptUser()
+    
+
